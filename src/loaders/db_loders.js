115318984241eade4e3bc2314a7f4dc24aaf9cd1
SBASKOY <salimbaskoy@gmail.com>
2021-12-15 12:16:39 +0300

    db connection

--- src/loaders/db_loders.js
+++ src/loaders/db_loders.js
@@ -6,7 +6,8 @@ db.once("open",()=>{
     console.log("db connection success");
 });
 const connectDb = async () => {
-    console.log(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-    await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+
+    console.log(process.env.DB_HOST_PUBLİSH || `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+    await Mongoose.connect(process.env.DB_HOST_PUBLİSH || `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
 }
 module.exports = connectDb;
